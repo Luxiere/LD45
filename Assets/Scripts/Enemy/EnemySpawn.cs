5c993b024d36dce0d4dab89af20ca86f733dc9f7
Luxiere <49752840+Luxiere@users.noreply.github.com>
2019-10-06 17:11:18 -0700

    small stuffs

---
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField]
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
