c259b2ae95491f57d1ef7623eb99dfac95a0462c
Luxiere <49752840+Luxiere@users.noreply.github.com>
2019-10-07 02:41:54 -0700

    commit?

--- Assets/PlantSpawn.cs
+++ Assets/PlantSpawn.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 public class PlantSpawn : MonoBehaviour
 {
     [SerializeField] GameObject[] plants = null;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void Spawning(int spawnIndex)
     {
