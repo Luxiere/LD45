16fd6d8e4218f3d675b2c642293e78b0792e040e
Luxiere <49752840+Luxiere@users.noreply.github.com>
2019-10-07 13:55:18 -0700

    wee woo bugs now

---
+++ Assets/MusicRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicRandomizer : MonoBehaviour
+{
+    [SerializeField] AudioClip[] clips;
+
+    private void Update()
+    {
+        if (!GetComponent<AudioSource>().isPlaying)
+        {
+            Randomize();
+        }
+    }
+
+    private void Randomize()
+    {
+        AudioClip clip = clips[Random.Range(0, clips.Length)];
+        GetComponent<AudioSource>().clip = clip;
+        GetComponent<AudioSource>().Play();
+    }
+}
